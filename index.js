c349aedf0b6be3dc22bb245848d41ae083f64826
leopalmeira <leandro2703palmeira@gmail.com>
2026-09-08 19:26:22 -0300

    feat: adicionar entrypoints index.js e src/index.js para deploy no Render

---
+++ index.js
@@ -0,0 +1,2 @@
+// Entrypoint raiz do DNA AUTO para plataformas de hospedagem em nuvem (Render, Railway, Heroku, etc.)
+require('./server/src/server.js');
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// Entrypoint de compatibilidade para inicialização automática no Render ("node src/index.js")
+require('../server/src/server.js');
